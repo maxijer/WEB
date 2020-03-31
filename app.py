4e4690721425349503540a16ccb140be61b2e3af
maxijer1234@yandex.ru <maxijer1234>
2020-03-31 11:54:52 +0300

    write parser and zapolnenie olimpiad

--- app.py
+++ app.py
@@ -306,4 +306,4 @@ def login():
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
